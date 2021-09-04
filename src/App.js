7728b6cd2e29e594d45a2505030ffbc9b7637d33
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-09-04 15:43:56 -0400

    done still needs auto scroll for incoming messages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import 'firebase/auth';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore'
 import React, { useRef, useState } from 'react';
+import { isFunctionExpression } from 'typescript';
 firebase.initializeApp({
   apiKey: "AIzaSyBznLlU-GFHbU2CYKho4oGN3Y0tWzOEqVw",
   authDomain: "line-6610e.firebaseapp.com",
@@ -20,88 +21,106 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-
+const functions = require('firebase-functions');
 
 
 function App() {
+
   const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <header>
-       
+        <h1>⚛️💬</h1>
+        <SignOut />
       </header>
+
       <section>
-        {user? <ChatRoom/> : <SignIn/>}
+        {user ? <ChatRoom /> : <SignIn />}
       </section>
+
     </div>
   );
 }
 
-function SignIn(){
+function SignIn() {
 
-  //pop up window
-  const signInWithGoogle = () =>{
+  const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
 
-  //sign in button
-  return(
-  <button onClick={signInWithGoogle}>Sign in with Google</button>)
-}
-function ChatRoom() {
-  const dummy = useRef();
-
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-  const [formValue, setFormValue] = useState('');
-  
-  const sendMessage = async(e) =>{
-    e.preventDefault();
-    const{uid, photoURL} = auth.currentUser;
-
-    messagesRef.add({
-       text: formValue,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-         uid, 
-         photoURL })
-
-    setFormValue('');
-    dummy.current.scrollIntoView({behavior: 'smooth'});
-  }
   return (
     <>
-      <main>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-        <div ref={dummy}></div>
-      </main>
-      <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
-        <button type="submit">💬</button>
-
-      </form>
-
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
   )
 
-
 }
 
-function SignOut(){
-  return auth.currentUser && ( <button onClick={()=> auth.signOut()}>Sign Out</button>)
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.limitToLast(25).orderBy('createdAt');
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+  const [formValue, setFormValue] = useState('');
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+    console.log("hahahahahaha")
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>💬</button>
+
+    </form>
+  </>)
+}
+
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : ' received';
-  return (<div className={`message ${messageClass}`}>
-    <img src={photoURL} />
-    <p>{text}</p>
-  </div>)
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
 }
+
 
 export default App;
